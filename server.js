153dfa2cafec04c7728c42e92290a0fbc1fc1c32
Utkarsh <mails.utkarsh28@gmail.com>
2020-03-27 23:30:50 +0530

    cors header added

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const PORT = process.env.PORT || 5000;
 require("./db")();
 const routes = require("./routes/index");
 const bodyParsor = require("body-parser");
-
+const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParsor());
-
+app.use(cors());
 app.use("/", routes);
 
 app.listen(PORT, () => {
